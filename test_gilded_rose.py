89ea3016db2a3400c78f94a6decd79a9b1be075c
Richeng Yang <richengyang@Richengs-MacBook-Pro.local>
2025-02-06 00:38:43 -0800

    Improve Lab4 - fix syntax error

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -22,14 +22,14 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(["Sulfuras"], all_items)
 
     # Test 1: (checks for logical errors)
-    def test_age_brie_should_not_decrease_quality(self):
-        items = [Item("Aged Brie", 10, 20)]
+    def test_sulfuras_should_not_decrease_sellin(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        aged_brie_item = items[0]
-        self.assertEqual(21, aged_brie_item.quality)
-        self.assertEqual(9, aged_brie_item.sell_in)
-        self.assertEqual("Aged Brie", aged_brie_item.name)
+        sulfuras_item = items[0]
+        self.assertEqual(79, sulfuras_item.quality)
+        self.assertEqual(5, sulfuras_item.sell_in)
+        self.assertEqual("Sulfuras", sulfuras_item.name)
 
     # Test 2: (checks for logical errors)
     def test_backstage_passes_should_not_decrease_quality_when_sellin_positive(self):
@@ -38,10 +38,10 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         backstage_passes_item = items[0]
         self.assertEqual(26, backstage_passes_item.quality)
-        self.assertEqual(9, backstage_passes_item.sell_in)
+        self.assertEqual(14, backstage_passes_item.sell_in)
         self.assertEqual("Backstage passes", backstage_passes_item.name)
     
-    # Test 3：(checks for logical errors)
+    # Test 3: (checks for logical errors)
     def test_conjured_items_should_degrade_twice_faster(self):
         items = [Item("Conjured", 10, 40)]
         gilded_rose = GildedRose(items)
@@ -51,7 +51,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(9, conjured_item.sell_in)
         self.assertEqual("Conjured", conjured_item.name)
     
-    # Test 4：(checks for logical errors)
+    # Test 4: (checks for logical errors)
     def test_backstage_passes_should_increase_2_quality_when_sellin_less_than_11(self):
         items = [Item("Backstage passes", 10, 25)]
         gilded_rose = GildedRose(items)
@@ -61,7 +61,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(9, backstage_passes_item.sell_in)
         self.assertEqual("Backstage passes", backstage_passes_item.name)
     
-    # Test 5：(checks for logical errors)
+    # Test 5: (checks for logical errors)
     def test_backstage_passes_should_increase_3_quality_when_sellin_less_than_6(self):
         items = [Item("Backstage passes", 5, 25)]
         gilded_rose = GildedRose(items)
@@ -71,7 +71,7 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(4, backstage_passes_item.sell_in)
         self.assertEqual("Backstage passes", backstage_passes_item.name)
 
-    # Test 6：(checks for syntax errors): use a function that not yet been created
+    # Test 6: (checks for syntax errors): use a function that not yet been created
     def test_gilded_rose_list_items_sell_in_less_than_5(self):
         items = [
             Item("Aged Brie", 3, 10),
